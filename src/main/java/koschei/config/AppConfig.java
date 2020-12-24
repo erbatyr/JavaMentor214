69f5594fe4657f32f917dc88563c0b22cbb335f3
erbatyr <erbatyr.akyn.97@gmail.com>
2020-12-24 21:11:04 +0600

    update

---
+++ src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,41 @@
+package koschei.config;
+
+import koschei.models.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+//    @Bean
+//    public static Island2 getIsland(Wood3 wood) {
+//        return new Island2(wood);
+//    }
+//
+//    @Bean
+//    public static Wood3 getWood(Rabbit4 rabbit) {
+//        return new Wood3(rabbit);
+//    }
+//
+//    @Bean
+//    public static Rabbit4 getRabbit(Duck5 duck) {
+//        return new Rabbit4(duck);
+//    }
+//
+//    @Bean
+//    public static Duck5 getDuck(Egg6 egg) {
+//        return new Duck5(egg);
+//    }
+//
+//    @Bean
+//    public static Egg6 getEgg(Needle7 needle) {
+//        return new Egg6(needle);
+//    }
+//
+//    @Bean
+//    public static Needle7 getNeedle(Deth8 deth) {
+//        return new Needle7(deth);
+//    }
+}
